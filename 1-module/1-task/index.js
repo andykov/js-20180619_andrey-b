56d53527f03639c1fac44975877e6ef8feb9465a
andykov <andykov.work@gmail.com>
2018-06-17 23:24:10 +0300

    Решена 1 задача модуля 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,11 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+    // var result = m
+    return alert(Math.pow(m, n));
 }
+
+var x = prompt('Введите число:');
+var n = prompt('Введите степень:');
+
+pow(x, n);
