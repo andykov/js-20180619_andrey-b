468f25fadf528154a9b43e69befe1723d1c6fc1b
andykov <abb.name@gmail.com>
2018-06-18 21:07:50 +0300

    Для продолжения на работе

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,13 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
+var obj = { test: 1 };
+
+
 function clone (obj) {
+
 }
+
+clone(obj) !== obj; // это разные объекты
+clone(obj).test === obj.test; // но содежат одни и те же поля
