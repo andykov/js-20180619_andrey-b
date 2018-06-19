4e8dd95ae2fe2d5c967de5ae225287d0de6a73c4
andykov <andykov.work@gmail.com>
2018-06-20 01:41:46 +0300

    fix error testing task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,12 +4,12 @@
  * @returns {Object}
  */
 
-var obj = { test: 1 };
-
-
-function clone (obj) {
-
-}
-
-clone(obj) !== obj; // это разные объекты
-clone(obj).test === obj.test; // но содежат одни и те же поля
+// var obj = { test: 1 };
+//
+//
+// function clone (obj) {
+//
+// }
+//
+// clone(obj) !== obj; // это разные объекты
+// clone(obj).test === obj.test; // но содежат одни и те же поля
