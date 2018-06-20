6c35cb51b0e62ea2b78babce5b2132b01bca6732
andykov <abb.name@gmail.com>
2018-06-20 18:53:33 +0300

    [solved] 3m-2t

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -2,8 +2,55 @@ let calendar = {
     from: new Date('2018-01-01T15:09:10Z'),
     to: new Date('2018-02-01T10:09:10Z')
 };
+let arrDay = [];
 
 calendar[Symbol.iterator] = function () {
+    let dateFrom = this.from,
+        dateTo = this.to,
+        dateFromMilliseconds = +dateFrom,
+        dateToMilliseconds = +dateTo,
+        saturday,
+        sunday,
+        result,
+        getDate;
+
+
+    return {
+        next() {
+            if (dateFromMilliseconds < +dateToMilliseconds) {
+
+                dateFromMilliseconds = dateFromMilliseconds + 86400000;
+                saturday = new Date(dateFromMilliseconds).toLocaleString("ru", {weekday: 'long'}) == 'суббота'; // есть ли еще способы найти выходной?
+                sunday = new Date(dateFromMilliseconds).toLocaleString("ru", {weekday: 'long'}) == 'воскресенье';
+                getDate = String(new Date(dateFromMilliseconds).getDate());
+
+                if (getDate < 10) {
+                    getDate = '0' + getDate;
+                }
+
+                result = getDate;
+
+                if (saturday || sunday) {
+                    result = '[' + getDate + ']';
+                }
+
+                return {
+                    done: false,
+                    value: result
+                };
+
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+    }
 };
 
+
+for (let day of calendar) {
+    arrDay.push(day);
+}
+console.log(arrDay);
 
