8f39eac7cddac5707063ddd8b97ef6172e6cf928
andykov <abb.name@gmail.com>
2018-06-21 15:20:51 +0300

    [solved] 4m-1t

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -6,4 +6,13 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList (friends) {
+    let ul = document.createElement('ul');
+
+    for (let user of friends) {
+        let li = document.createElement('li');
+        li.innerHTML = user.firstName + ' ' + user.lastName;
+        ul.appendChild(li);
+    }
+
+    return ul
 }
