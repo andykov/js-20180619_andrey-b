bc95b975b40bd44093ee448e334bf3cf3e1977dd
andykov <andykov.work@gmail.com>
2018-06-27 00:04:45 +0300

    [solved] 2m-1t

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,13 +3,24 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+function clone(obj) {
+    if (typeof obj === 'object' && obj !== null) {
+        let objClone = {};
 
-// var obj = { test: 1 };
-//
-//
-// function clone (obj) {
-//
-// }
-//
-// clone(obj) !== obj; // это разные объекты
-// clone(obj).test === obj.test; // но содежат одни и те же поля
+        for (var key in obj) {
+            objClone.prop = 'value';
+            objClone[key] = obj[key];
+        }
+
+        return objClone;
+    }
+};
+
+let obj = {test: { test2: { test3: null } }};
+
+console.log(clone(obj) !== obj); // это разные объекты
+console.log(clone(obj).test === obj.test); // но содежат одни и те же поля
+console.log(clone(obj).prop === obj.prop); // но содежат одни и те же поля
+
+console.log(clone(obj));
+console.log(typeof clone(obj).test.test2 === 'object' && typeof obj.test.test2 === 'object');
