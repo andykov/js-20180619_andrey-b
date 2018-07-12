1d17be8c5e37488f54a10c3553cbf5e6d3fe0f48
andykov <andykov.work@gmail.com>
2018-07-12 23:34:22 +0300

    [solved] 5m-1t

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,29 @@
  * @param {Element} table
  */
 function highlight(table) {
+    let arr = Array.from(table.querySelectorAll('tbody tr'));
+
+    arr.forEach(function (row, index) {
+
+        if (row.children[3].hasAttribute('data-available')) {
+            if (row.children[3].dataset.available === 'true') {
+                row.classList.add('available');
+            } else if (row.children[3].dataset.available === 'false') {
+                row.classList.add('unavailable');
+            }
+        } else {
+            row.hidden = true;
+        }
+
+        if (row.children[2].innerHTML === 'm') {
+            row.classList.add('male');
+        } else if (row.children[2].innerHTML === 'f') {
+            row.classList.add('female');
+        }
+
+        if (Number(row.children[1].innerHTML) < 18) {
+            row.style.textDecoration = 'line-through';
+        }
+
+    });
 }
